d84168889ebf23bcde112e0608f7b76aba095de4
Vladimir Tikhonov <vladimir@tikhonov.by>
2016-11-17 21:37:10 +0300

    Fix eslint config after renaming utils -> tools

--- tools/generate_manifest.js
+++ tools/generate_manifest.js
@@ -1,6 +1,7 @@
-const manifest = require('../configs/manifest.json');
 const fileSystem = require('fs');
 const path = require('path');
+
+const manifest = require('../configs/manifest.json');
 
 manifest.description = process.env.npm_package_description;
 manifest.version = process.env.npm_package_version;
